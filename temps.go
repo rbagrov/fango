e221a1ffb7feaefd25b6b40dd8084837c38cb277
Rostislav Bagrov <bagrov.rostislav@gmail.com>
2019-08-22 12:26:29 +0300

    issue/4
    Signed-off-by: Rostislav Bagrov <bagrov.rostislav@gmail.com>

--- temps.go
+++ temps.go
@@ -5,10 +5,18 @@ import (
 	fmt "fmt"
 	termui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+	cpu "github.com/shirou/gopsutil/cpu"
 	host "github.com/shirou/gopsutil/host"
+	load "github.com/shirou/gopsutil/load"
 	log "log"
 	time "time"
 )
+
+// GetCPUInfo gets cpu information
+func GetCPUInfo() string {
+	info, _ := cpu.Info()
+	return info[0].ModelName
+}
 
 // GetTemperature return cpu temp
 func GetTemperature() int {
@@ -35,6 +43,12 @@ func GetColorByTemp(temp int) termui.Color {
 	return color
 }
 
+// GetLoad1 returns load for the past 1 minute
+func GetLoad1() float64 {
+	info, _ := load.Avg()
+	return info.Load1
+}
+
 // LocalMonitor start local measurement of temperature
 func LocalMonitor() {
 	if err := termui.Init(); err != nil {
@@ -42,10 +56,18 @@ func LocalMonitor() {
 	}
 	defer termui.Close()
 
+	cpuInfo := GetCPUInfo()
+
+	var barLength int
+
+	if 2*len(cpuInfo) < 60 {
+		barLength = 60
+	} else {
+		barLength = 2 * len(cpuInfo)
+	}
+
 	g := widgets.NewGauge()
-	g.Title = "CPU temp"
-	g.Percent = 0
-	g.SetRect(0, 0, 50, 3)
+	g.SetRect(0, 0, barLength, 3)
 	g.BarColor = termui.ColorGreen
 	g.BorderStyle.Fg = termui.ColorWhite
 	g.TitleStyle.Fg = termui.ColorWhite
@@ -54,6 +76,7 @@ func LocalMonitor() {
 		temp := GetTemperature()
 		g.Percent = temp
 		g.Label = fmt.Sprintf("%v C", temp)
+		g.Title = fmt.Sprintf("%v Temperature @ Load: %.2f", cpuInfo, GetLoad1())
 		g.BarColor = GetColorByTemp(temp)
 		termui.Render(g)
 	}
